34a75369f0661444023b6477dcc43133a2b2b14e
Mark <mverleg.noreply@gmail.com>
2019-05-18 17:26:52 +0200

    Create a Dockerfile that does not cache dependencies

--- benchmark.sh
+++ benchmark.sh
@@ -9,7 +9,7 @@ rustup install "$RUST_VERSION"
 echo ">> creating rust nightly docker image"
 docker build --quiet --file Dockerfile.nightly --tag bench_rust_nightly .
 
-REPS=3
+REPS=1
 for i in `seq 1 $REPS`
 do
     echo ">> iteration $i of $REPS"
@@ -19,6 +19,6 @@ do
     \time --format "native: real %e s | user %U s | sys %S s | mem %K kb" -- cargo "+$RUST_VERSION" build --quiet --all --release
 
     # Docker
-    \time --format "docker: real %e s | user %U s | sys %S s | mem %K kb" -- docker build --no-cache --quiet .
+    \time --format "docker: real %e s | user %U s | sys %S s | mem %K kb" -- docker build --no-cache --quiet --file Dockerfile.nocache .
 
 done
