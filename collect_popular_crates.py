7d3bbf7f6c240bd042c2e9689b1dff66d6d1c3a9
Mark <mverleg.noreply@gmail.com>
2019-05-18 17:24:42 +0200

    Reduce the number of crates further

--- collect_popular_crates.py
+++ collect_popular_crates.py
@@ -2,9 +2,9 @@ from urllib.request import urlopen
 from json import loads
 from sys import stderr
 
-PAGES = 1
+PAGES = 3
 for page in range(0, PAGES):
-	url = 'https://crates.io/api/v1/crates?page={0:d}&per_page=100&sort=recent-downloads'.format(page + 1)
+	url = 'https://crates.io/api/v1/crates?page={0:d}&per_page=20&sort=recent-downloads'.format(page + 1)
 	with urlopen(url) as uh:
 		stderr.write('page {} of {}\n'.format(page + 1, PAGES))
 		data = loads(uh.read().decode())
