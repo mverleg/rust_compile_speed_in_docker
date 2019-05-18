89eb1d97072a2b3e72162b5e35c611312800e3ce
Mark <mverleg.noreply@gmail.com>
2019-05-18 16:13:52 +0200

    Create python script to collect popular dependencies

---
+++ collect_popular_crates.py
@@ -0,0 +1,14 @@
+from urllib.request import urlopen
+from json import loads
+from sys import stderr
+
+PAGES = 3  # TODO @mark: TEMPORARY! REMOVE THIS!
+for page in range(0, PAGES):
+	url = 'https://crates.io/api/v1/crates?page={0:d}&per_page=100&sort=recent-downloads'.format(page + 1)
+	with urlopen(url) as uh:
+		stderr.write('page {} of {}\n'.format(page + 1, PAGES))
+		data = loads(uh.read().decode())
+		for crate in data['crates']:
+			print('{} = {{version = "{}"}}'.format(crate['id'], crate['max_version']))
+		
+	
