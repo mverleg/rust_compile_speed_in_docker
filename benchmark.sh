6c48874e5933f1e08fc16a00d8eadc663b6946bf
Mark <mverleg.noreply@gmail.com>
2019-05-18 16:39:11 +0200

    Create a benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -1,2 +1,19 @@
 #!/usr/bin/env bash
 
+echo ">> installing correct rust version"
+RUST_VERSION="nightly-2019-05-15"
+rustup install "$RUST_VERSION"
+
+echo ">> creating rust nightly docker image"
+docker build --file Dockerfile.nightly --tag bench_rust_nightly .
+
+for i in `seq 1 1`
+do
+    echo ">> iteration $i"
+
+    echo ">> native"
+    \time --format "user %U s | sys %S s | mem %K kb" -- cargo "+$RUST_VERSION" build --all --release
+
+    echo ">> docker"
+    \time --format "user %U s | sys %S s | mem %K kb" -- docker build .
+done
